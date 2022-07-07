e1e2c83a32e9370ec87bb419185bed8e8d7da95a
iandev096 <68153591+iandev096@users.noreply.github.com>
2022-07-07 13:31:20 +0000

    removed hotjar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,28 +7,19 @@ class MyDocument extends Document {
   }
 
   render() {
-    (function (h, o, t, j, a, r) {
-      h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
-      h._hjSettings = { hjid: 3056369, hjsv: 6 };
-      a = o.getElementsByTagName('head')[0];
-      r = o.createElement('script'); r.async = 1;
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-      a.appendChild(r);
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-
     return (
       <Html className="scroll-smooth">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" /> 
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> 
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,500;0,600;0,700;1,100;1,300;1,600;1,700&display=swap" rel="stylesheet" />
 
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> 
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,500;0,600;0,700;1,100;1,300;1,600;1,700&family=Spline+Sans+Mono:wght@400;500;600&display=swap" rel="stylesheet" />
         </Head>
-        <body
-        // className="debug-screens"
-        >
+        <body 
+          // className="debug-screens"
+          >
           <Main />
           <NextScript />
         </body>
