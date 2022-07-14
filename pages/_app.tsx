69b3a6bb87546db137f175d8e2630f0689b30fbf
Isaac Anyimiah <iandev096@gmail.com>
2022-07-14 16:35:10 +0000

    removed hotjar feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextPage } from "next";
 import { ReactElement, ReactNode, useEffect } from "react";
-import initializeHotjar from "../util/hotjar";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -14,10 +13,6 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
-
-  useEffect(() => {
-    initializeHotjar();
-  }, []);
 
   return <>{getLayout(<Component {...pageProps} />)}</>;
 }
